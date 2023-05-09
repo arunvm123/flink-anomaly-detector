adc6dff3ec88a417692990ea4f0d4ec0af674a23
Arun Mathew <arunvmathew95@gmail.com>
2023-05-09 20:21:56 +0530

    Update Request class to have only user id

--- src/main/java/sample/DataStreamJob.java
+++ src/main/java/sample/DataStreamJob.java
@@ -27,19 +27,6 @@ import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsIni
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 public class DataStreamJob {
 
 	public static void main(String[] args) throws Exception {
--- src/main/java/sample/Request.java
+++ src/main/java/sample/Request.java
@@ -1,20 +1,9 @@
 package sample;
 
 public class Request {
-        public Request() {
-        }
-
-        public Request(String UserID) {
-                this.UserID = UserID;
-        }
+        public String UserID;
 
         public String getUserID() {
                 return UserID;
         }
-
-        public void setUserID(String userID) {
-                UserID = userID;
-        }
-
-        public String UserID;
 }
